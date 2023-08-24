a06dfb90c603350c210c83e6d79d7bd2e96234df
Adriano Barbosa <adriano.barbosa100@email.com>
2023-08-24 15:33:08 -0300

    feaT: change variables

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { config } from "./config/config";
 
 const start = async () => {
   console.log("add console");
-  if (!process.env.JWT_KEY) {
+  if (!config.JWT_KEY) {
     throw new Error("JWT_KEY deve ser definido!");
   }
 
-  if (!process.env.MONGO_URI) {
+  if (!config.MONGODB_URL) {
     throw new Error("MONGO_URI deve ser definido!");
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(config.MONGODB_URL);
     console.log("[INFO] Connected to MongoDB");
 
-    app.listen(3000, () => {
-      console.log("[INFO] Listening on port 3000!");
+    app.listen(config.PORT, () => {
+      console.log(`[INFO] Listening on port ${config.PORT}!`);
     });
   } catch (err) {
     console.error(err);
